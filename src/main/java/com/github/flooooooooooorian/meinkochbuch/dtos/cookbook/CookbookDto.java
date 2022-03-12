8203b43504a9a6a680595529527236a7cabc48ce
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-03-12 23:56:44 +0100

    Add CookbookDto, Mapper, tests

--- src/main/java/com/github/flooooooooooorian/meinkochbuch/dtos/cookbook/CookbookDto.java
+++ src/main/java/com/github/flooooooooooorian/meinkochbuch/dtos/cookbook/CookbookDto.java
@@ -2,25 +2,26 @@ package com.github.flooooooooooorian.meinkochbuch.dtos.cookbook;
 
 import com.github.flooooooooooorian.meinkochbuch.dtos.ImageDto;
 import com.github.flooooooooooorian.meinkochbuch.dtos.chefuser.ChefUserPreviewDto;
+import com.github.flooooooooooorian.meinkochbuch.dtos.recipe.RecipePreviewDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CookbookPreview {
+public class CookbookDto {
 
     private String id;
     private String name;
-
+    private boolean privacy;
+    private List<RecipePreviewDto> recipes;
     private ChefUserPreviewDto owner;
-
     private ImageDto thumbnail;
-
     private BigDecimal ratingAverage;
 }
