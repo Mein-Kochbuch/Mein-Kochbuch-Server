9adf4906d637f7ebb9ccf8e2f50182a80b74c5b7
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-07-04 09:05:48 +0200

    WIP IngredientMatching

--- src/main/java/com/github/flooooooooooorian/meinkochbuch/repository/BaseIngredientRepository.java
+++ src/main/java/com/github/flooooooooooorian/meinkochbuch/repository/BaseIngredientRepository.java
@@ -4,7 +4,12 @@ import com.github.flooooooooooorian.meinkochbuch.models.recipe.ingredient.BaseIn
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BaseIngredientRepository extends JpaRepository<BaseIngredient, String> {
     boolean existsByMigrationId(int id);
+
+    //BaseIngredient findFirstByNameIgnoreCaseOrSingularIgnoreCaseOrSynonymsIsContainingIgnoreCase(String text);
+    BaseIngredient findFirstByNameInOrSingularInOrSynonymsContainingIgnoreCase(List<String> texts);
 }
